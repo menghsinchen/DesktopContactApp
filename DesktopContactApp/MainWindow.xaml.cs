7d8947f9d6dd409fb58dc7b5a8378e134cc4b27f
Meng-Hsin Chen <walkproudly@gmail.com>
2024-02-08 18:32:58 +0800

    Update MainWindow
    1. Add ListView in MainWindow
    2. Bind Contact with ListView Items
    3. Add search textbox to filter string from contact Name

--- DesktopContactApp/MainWindow.xaml.cs
+++ DesktopContactApp/MainWindow.xaml.cs
@@ -18,17 +18,36 @@ namespace DesktopContactApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Contact> allContacts = new List<Contact>();
+
         public MainWindow()
         {
             InitializeComponent();
-            SQLiteHelper.ReadContact();
+            RefreshContacts();
         }
 
         private void btnCreateNewContact_Click(object sender, RoutedEventArgs e)
         {
             NewContactWindow newContactWindow = new NewContactWindow();
             newContactWindow.ShowDialog();
-            SQLiteHelper.ReadContact();
+            RefreshContacts();
+        }
+
+        private void txbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterContacts();
+        }
+
+        private void RefreshContacts()
+        {
+            allContacts = SQLiteHelper.ReadContact().OrderBy(x => x.Name).ToList();
+            lvContacts.ItemsSource = allContacts;
+        }
+
+        private void FilterContacts()
+        {
+            List<Contact> filteredContacts = allContacts.Where(x => x.Name.Contains(txbSearch.Text, StringComparison.OrdinalIgnoreCase)).ToList();
+            lvContacts.ItemsSource = filteredContacts;
         }
     }
 }
